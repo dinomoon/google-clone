8d8cd18e0be4f0ceb7454caef288f3dcf22b6dc3
MoonK <ansrud45@gmail.com>
2021-10-20 13:31:18 +0900

    완성

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import { ResultContextProvider } from './contexts/ResultContextProvider';
 import './global.css';
 
-reactDom.render(<App />, document.getElementById('root'));
+reactDom.render(
+  <ResultContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ResultContextProvider>,
+  document.getElementById('root'),
+);
